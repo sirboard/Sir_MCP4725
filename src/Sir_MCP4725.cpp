50314d29e126e68dc376ba022c5bb664a26762d2
Imran Paruk <imranparuk@live.com>
2020-01-22 00:31:35 +0200

    updates

--- src/Sir_MCP4725.cpp
+++ src/Sir_MCP4725.cpp
@@ -8,8 +8,29 @@
 #include "Sir_MCP4725.h"
 
 
-Sir_MCP4725::Sir_MCP4725() {
+Sir_MCP4725::Sir_MCP4725(uint8_t addr) {
 }
 
 void Sir_MCP4725::init(uint8_t addr) {
+  i2caddr = addr;
+  Wire.begin();
+}
+
+void Sir_MCP4725::setVoltage(uint16_t x, bool writeEEPROM )
+{
+  uint8_t xlow = x & 0xff;
+  uint8_t xhigh = (x >> 8);
+#ifdef TWBR
+  uint8_t twbrback = TWBR;
+  TWBR = ((F_CPU / 400000L) - 16) / 2; // Set I2C frequency to 400kHz
+#endif
+  Wire.beginTransmission(i2caddr);
+  if (writeEEPROM) Wire.write(WRITEDACEEPROM_CMD);
+  else Wire.write(WRITEDAC_CMD);
+  Wire.write(xlow);                   
+  Wire.write(xhigh);            
+  Wire.endTransmission();
+#ifdef TWBR
+  TWBR = twbrback;
+#endif
 }
